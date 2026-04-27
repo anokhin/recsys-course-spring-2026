7192855dc374b463d078bdb07bbd1202a1e89ced
Iaroslava Seregina <yaroslavaseregina4@gmail.com>
2026-04-27 15:06:47 +0300

    hw2: fix missing import

--- botify/botify/server.py
+++ botify/botify/server.py
@@ -16,7 +16,6 @@ from botify.recommenders.i2i import I2IRecommender
 from botify.recommenders.random import Random
 from botify.recommenders.indexed import Indexed
 from botify.recommenders.sticky_artist import StickyArtist
-from botify.recommenders.hstu_lightfm_reranker import HSTUwithLightFMReranker
 from botify.recommenders.hstu_sasrec_reranker import HSTUwithSasRecReranker
 from botify.track import Catalog
 
