9373a277e2b6f68601121da214707d939bf9a33e
Robolightning <irobolightning@gmail.com>
2026-04-28 18:51:58 +0300

    Add conservative distilled ensemble for hw2

--- botify/botify/server.py
+++ botify/botify/server.py
@@ -16,6 +16,7 @@ from botify.recommenders.i2i import I2IRecommender
 from botify.recommenders.random import Random
 from botify.recommenders.indexed import Indexed
 from botify.recommenders.sticky_artist import StickyArtist
+from botify.recommenders.ensemble import EnsembleRecommender
 from botify.track import Catalog
 
 root = logging.getLogger()
@@ -32,6 +33,8 @@ recommendations_lfm_redis = Redis(app, config_prefix="REDIS_RECOMMENDATIONS_LFM"
 recommendations_contextual_redis = Redis(app, config_prefix="REDIS_RECOMMENDATIONS_SASREC")
 
 recommendations_hstu_redis = Redis(app, config_prefix="REDIS_RECOMMENDATIONS_HSTU")
+recommendations_ensemble_user_redis = Redis(app, config_prefix="REDIS_RECOMMENDATIONS_ENSEMBLE_USER")
+recommendations_ensemble_i2i_redis = Redis(app, config_prefix="REDIS_RECOMMENDATIONS_ENSEMBLE_I2I")
 
 data_logger = DataLogger(app)
 atexit.register(data_logger.close)
@@ -72,6 +75,28 @@ sasrec_i2i_recommender = I2IRecommender(
     listen_history_redis.connection,
     recommendations_contextual_redis.connection,
     random_recommender,
+)
+
+catalog.upload_recommendations(
+    recommendations_ensemble_user_redis.connection,
+    "RECOMMENDATIONS_ENSEMBLE_USER_FILE_PATH",
+    key_object="user",
+    key_recommendations="tracks",
+)
+
+catalog.upload_recommendations(
+    recommendations_ensemble_i2i_redis.connection,
+    "RECOMMENDATIONS_ENSEMBLE_I2I_FILE_PATH",
+    key_object="item_id",
+    key_recommendations="recommendations",
+)
+
+ensemble_recommender = EnsembleRecommender(
+    listen_history_redis.connection,
+    recommendations_ensemble_user_redis.connection,
+    recommendations_ensemble_i2i_redis.connection,
+    recommendations_contextual_redis.connection,
+    sasrec_i2i_recommender,
 )
 
 parser = reqparse.RequestParser()
@@ -117,7 +142,7 @@ class NextTrack(Resource):
         if treatment == Treatment.C:
             recommender = sasrec_i2i_recommender
         elif treatment == Treatment.T1:
-            recommender = Indexed(recommendations_hstu_redis.connection, catalog, random_recommender)
+            recommender = ensemble_recommender
         else:
             recommender = random_recommender
 
